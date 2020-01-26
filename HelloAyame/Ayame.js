06da70b34ae8fc715f12f83a5b1aef57cedf7f5b
enm10k <enm10k@gmail.com>
2020-01-26 20:17:15 +0900

    isExistClient が true の場合のみ offer を送る

--- HelloAyame/Ayame.js
+++ HelloAyame/Ayame.js
@@ -203,7 +203,7 @@ export class Ayame extends AyameEventTarget {
             this.configuration.iceServers = iceServers;
           }
           if(!this._pc) this._pc = await this._createPeerConnection();
-          await this._sendOffer();
+          if (signal.isExistClient) await this._sendOffer();
           break;
         case 'reject':
           logger.log('# Ayame: rejected', signal);
