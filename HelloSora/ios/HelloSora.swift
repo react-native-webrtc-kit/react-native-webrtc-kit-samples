2c2c816dc8c82a8b5e08ef85b674843421309096
enm10k <enm10k@gmail.com>
2020-06-27 21:22:15 +0900

    HelloSora: Xcode のビルド・エラーを修正する

---
+++ HelloSora/ios/HelloSora.swift
@@ -0,0 +1,11 @@
+//
+//  HelloSora.swift
+//  HelloSora
+//
+//  Created by enm10k on 2020/06/28.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
+
+// NOTE(enm10k): https://github.com/react-native-community/lottie-react-native/issues/531 のエラーを解消するために、空の .swift ファイルの追加が必要だった
