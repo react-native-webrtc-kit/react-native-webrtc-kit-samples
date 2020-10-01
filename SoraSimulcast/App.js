62476b7d5c874eac36a21c8e4ea2366c39122857
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2020-10-01 17:07:30 +0900

    sendonly の videoview を配置する

--- SoraSimulcast/App.js
+++ SoraSimulcast/App.js
@@ -1,114 +1,223 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+// @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
+  Platform,
   StyleSheet,
-  ScrollView,
   View,
-  Text,
-  StatusBar,
+  PermissionsAndroid
 } from 'react-native';
-
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  Text,
+  TextInput,
+  Switch,
+  Button,
+} from 'react-native-paper';
+import {
+  WebRTC,
+  RTCVideoView,
+  RTCObjectFit,
+  RTCLogger as logger,
+  RTCMediaStreamTrack,
+} from 'react-native-webrtc-kit';
+import { Sora } from './Sora';
+import { url, defaultChannelId, signalingKey } from './app.json';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+if (Platform.OS === 'ios') {
+  WebRTC.setMicrophoneEnabled(true);
+}
+
+logger.setDebugMode(true);
+
+type Props = {};
+
+type State = {
+  channelId: string,
+  multistream: bool,
+  pubConn: Sora | null,
+  senderTrack: RTCMediaStreamTrack | null;
+  objectFit: RTCObjectFit
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+async function requestPermissionsAndroid() {
+  try {
+    await PermissionsAndroid.requestMultiple(
+      [
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+      ]
+    );
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+
+export default class App extends Component<Props, State> {
+
+  constructor(props: Object) {
+    super(props);
+    this.state = {
+      channelId: defaultChannelId,
+      multistream: false,
+      pubConn: null,
+      senderTrack: null,
+      objectFit: 'cover'
+    };
+  }
+
+  componentDidMount() {
+    // Android の場合カメラの権限をリクエストする
+    // XXX(kdxu): 厳密には拒否された場合の処理がいるはず。
+    if (Platform.OS === 'android') {
+      requestPermissionsAndroid()
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.body}>
+        <View style={styles.div_content}>
+          <Text style={styles.instructions}>
+            {instructions}
+          </Text>
+          <View style={styles.div_header}>
+          {(this.state.pubConn !== null && this.state.senderTrack !== null) &&
+            <RTCVideoView
+              style={styles.videoview}
+              track={this.state.senderTrack}
+              objectFit={this.state.objectFit}
+            />
+          }
+          </View>
+          <View style={{ flex: 1, flexDirection: 'column' }}>
+            <TextInput
+              label="チャネルID"
+              mode="outlined"
+              style={{
+                width: '100%',
+                height: 60,
+                borderColor: 'gray'
+              }}
+              onChangeText={(channelId) =>
+                  this.setState({ channelId: channelId })
+              }
+              value={this.state.channelId}
+              placeholder='Channel ID'
+            />
+            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+              <Switch
+                value={this.state.multistream}
+                onValueChange={() => {
+                  this.setState({ multistream: !this.state.multistream });
+                }}
+              />
+              <Text
+                onPress={(_value) => {
+                  this.setState({ multistream: !this.state.multistream });
+                }}
+              >
+                マルチストリーム
+              </Text>
+            </View>
+          </View>
+          <View>
+            <Button
+              disabled={this.state.pubConn !== null}
+              raised
+              mode="outlined"
+              onPress={() => {
+                this.setState(prev => {
+                  const role = "sendonly";
+                  const pubConn = new Sora(url, role, this.state.multistream, prev.channelId, signalingKey);
+                  pubConn.onconnectionstatechange = function (event) {
+                    this.setState(prev => {
+                      logger.log("# publisher connection state change => ",
+                        event.target.connectionState);
+                      if (event.target.connectionState == 'connected') {
+                        var sender = prev.pubConn._pc.senders.find(each => {
+                          return each.track.kind == 'video'
+                        });
+                        logger.log("# publisher connection connected =>", sender);
+                        return { senderTrack: sender.track }
+                      }
+                    });
+                  }.bind(this);
+                  pubConn.connect();
+                  return { pubConn: pubConn };
+                });
+              }}
+            >
+              接続する
+            </Button>
+            <Button
+              raised
+              mode="outlined"
+              onPress={() => {
+                logger.log("# disconnect");
+                if (this.state.pubConn) {
+                  this.state.pubConn.disconnect();
+                }
+                this.setState(prev => {
+                  return {
+                    pubConn: null,
+                    senderTrack: null
+                  }
+                });
+              }}
+            >
+              接続解除する
+            </Button>
+          </View>
+        </View>
+      </View >
+    );
+  }
+}
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
 });
 
-export default App;
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    backgroundColor: '#F5FCFF',
+    padding: 30
+  },
+  div_header: {
+    width: '100%',
+    aspectRatio: 16.0 / 9.0,
+    backgroundColor: 'black',
+    elevation: 4,
+    marginBottom: 10
+  },
+  div_content: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 24,
+    paddingHorizontal: 8,
+    paddingBottom: 8,
+  },
+  videoview: {
+    flex: 1,
+    backgroundColor: 'lightgray',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
